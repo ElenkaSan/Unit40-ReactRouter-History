9723a95b6dacc429f329c61d652be9206c4e99e1
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-05-03 16:03:40 -0400

    adding remove btn

--- 41.2exs react-router-patterns/part2-color-factory/src/Routes.js
+++ 41.2exs react-router-patterns/part2-color-factory/src/Routes.js
@@ -3,18 +3,22 @@ import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import ColorDetails from './ColorDetails';
 import ColorList from './ColorList';
 import ColorForm from './ColorForm';
+import { v4 as uuid } from "uuid";
 
 const Routes = () => {
     const initialState = [
       {
+        id: uuid(),
         name  : 'Purple',
         color : 'purple'
       },
-      {
+      { 
+        id: uuid(),
         name  : 'Blue',
         color : 'rgb(31, 169, 255)'
       },
-      {
+      { 
+        id: uuid(),
         name  : 'Almost green',
         color : 'rgb(73, 198, 169)'
       }
@@ -24,11 +28,14 @@ const Routes = () => {
     const addColor = ({ name, color }) => {
       setColors((colors) => [ ...colors, { name: name, color: color } ]);
     };
+    const removeColor = ({ id }) => {
+      setColors((colors) => colors.filter((color)=> color.id != id));
+    };
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/colors">
-            <ColorList colors={colors} />
+            <ColorList colors={colors} removeColor={removeColor} />
           </Route>
           <Route exact path="/colors/new">
             <ColorForm addColor={addColor} />
