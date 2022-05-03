5ebf0b50ed13636f9edf2d7d39ee8c79a501d87c
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-05-03 16:09:18 -0400

    adding remove btn

--- 41.2exs react-router-patterns/part2-color-factory/src/ColorList.js
+++ 41.2exs react-router-patterns/part2-color-factory/src/ColorList.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ColorList.css';
 
-function ColorList({colors}) {
+function ColorList({colors, removeColor}) {
   return (
 		<div>
 			<div className='ColorList'>
 				<h1 className='ColorList-h'>Welcome to the color factory.</h1>
-				<hy3>
+				<h3>
 					<Link to="/colors/new">Add a color</Link>{' '}
-				</hy3>
+				</h3>
 			</div>
 			<div>
 				<h2>Please select a color.</h2>
 				{colors.map((color) => (
+					<div id={color.id}>
 					<h3 className='CL'>
 						<Link to={`/colors/${color.name}`}>Hey! Here is {color.name}.</Link>
 					</h3>
+					<button className='CL-button' onClick={() =>removeColor({ id: color.id })}> X </button>
+					</div>
 				))}
 			</div>
 		</div>
